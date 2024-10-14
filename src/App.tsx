60c0a89b971ea9d2d4fe3661a1ffc17dbbaa345d
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-14 13:43:35 +0200

    wip build GameBoard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import GameBoard from "./components/game-board";
 import SettingsBoard from "./components/settings-board";
 
 function App() {
   return (
     <section className="flex w-full max-w-[327px] flex-col gap-8">
-      <SettingsBoard />
+      {/* <SettingsBoard /> */}
+      <GameBoard />
     </section>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,21 +3,29 @@ import { cn } from "../lib/utils";
 
 type ButtonProps = {
   color: "mango" | "turquoise" | "gray";
+  variant?: "primary" | "secondary";
   children: React.ReactNode;
   className?: string;
 };
 
-export default function Button({ children, className, color }: ButtonProps) {
+export default function Button({
+  children,
+  className,
+  color,
+  variant = "primary",
+}: ButtonProps) {
   return (
     <button
       className={cn(
-        "rounded-2xl py-3 font-bold text-navy transition-colors duration-100 ease-in-out",
+        "flex items-center justify-center rounded-2xl py-3 font-bold text-navy transition-colors duration-100 ease-in-out",
         {
           "bg-mango-base shadow-[0_8px_#CC8B13] hover:bg-mango-light":
             color === "mango",
           "bg-turquoise-base shadow-[0_8px_#118C87] hover:bg-turquoise-light":
             color === "turquoise",
           "bg-gray shadow-[0_8px_#6B8997] hover:bg-white": color === "gray",
+          "aspect-square rounded-md shadow-[0_4px_#6B8997]":
+            variant === "secondary",
         },
         className,
       )}
