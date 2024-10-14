b367ddd69205a4bf612fbbafb6ce2e17cc1d8e97
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-14 15:17:21 +0200

    Refactor icon files

--- src/components/game-board.tsx
+++ src/components/game-board.tsx
@@ -1,6 +1,6 @@
-import Button from "./button";
 import Logo from "./logo";
 import IconX from "../assets/icons/icon-x";
+import IconRestart from "../assets/icons/icon-restart";
 
 export default function GameBoard() {
   return (
@@ -11,23 +11,9 @@ export default function GameBoard() {
           <IconX className="size-4 fill-green-500" />
           turn
         </div>
-        <Button
-          color="gray"
-          variant="secondary"
-          className="size-[40px] justify-self-end"
-        >
-          <svg
-            width="15"
-            height="15"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path
-              d="M19.524 0h-1.88a.476.476 0 0 0-.476.499l.159 3.284A9.81 9.81 0 0 0 9.835.317C4.415.317-.004 4.743 0 10.167.004 15.597 4.406 20 9.835 20a9.796 9.796 0 0 0 6.59-2.536.476.476 0 0 0 .019-.692l-1.348-1.349a.476.476 0 0 0-.65-.022 6.976 6.976 0 0 1-9.85-.63 6.987 6.987 0 0 1 .63-9.857 6.976 6.976 0 0 1 10.403 1.348l-4.027-.193a.476.476 0 0 0-.498.476v1.881c0 .263.213.476.476.476h7.944A.476.476 0 0 0 20 8.426V.476A.476.476 0 0 0 19.524 0Z"
-              fill="#1F3641"
-            ></path>
-          </svg>
-        </Button>
+        <button className="flex size-[40px] items-center justify-center justify-self-end rounded-md bg-gray">
+          <IconRestart className="size-4" />
+        </button>
       </div>
       <div>playboard</div>
       <div className="grid grid-cols-3 gap-2">
