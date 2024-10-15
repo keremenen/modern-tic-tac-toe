9560acb33b79101e7d3b4ce74ff02cb3fc69ae4d
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-10-15 11:29:31 +0200

    Remove unused utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,12 +5,3 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function extractFillClass(
-  className: string | undefined,
-): string | undefined {
-  if (!className) return undefined
-
-  const fillClass = className.split(' ').find((cls) => cls.startsWith('fill'))
-  return fillClass
-}
